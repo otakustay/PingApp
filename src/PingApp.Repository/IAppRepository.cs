7818f183e553a58b49eb066d45a1762d1f2449cc
otakustay <otakustay@gmail.com>
2012-03-30 16:42:28 +0800

    Add interface required for Schedule

---
+++ src/PingApp.Repository/IAppRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PingApp.Entity;
+
+namespace PingApp.Repository {
+    public interface IAppRepository {
+
+        // 以下为Schedule使用
+        ISet<int> FindExists(IEnumerable<int> apps);
+
+        ICollection<App> Retrieve(IEnumerable<int> required);
+
+        void Save(App app);
+
+        void Update(App app);
+    }
+}
---
+++ src/PingApp.Repository/IAppUpdateRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PingApp.Entity;
+
+namespace PingApp.Repository {
+    public interface IAppUpdateRepository {
+
+        // 以下为Schedule使用
+        void Save(AppUpdate update);
+    }
+}
