20782dd5864238e9a52a2b6266f25c7688eb141e
otakustay <otakustay@gmail.com>
2012-03-30 17:07:21 +0800

    Add initial NHibernate implement for Repository interface.

---
+++ src/PingApp.Repository.NHibernate/AppRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingApp.Repository.NHibernate {
+    public class AppRepository : IAppRepository {
+        public ISet<int> FindExists(IEnumerable<int> apps) {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Entity.App> Retrieve(IEnumerable<int> required) {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Entity.App app) {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Entity.App app) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/PingApp.Repository.NHibernate/AppUpdateRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PingApp.Repository.NHibernate {
+    public class AppUpdateRepository : IAppUpdateRepository {
+        public void Save(Entity.AppUpdate update) {
+            throw new NotImplementedException();
+        }
+    }
+}
