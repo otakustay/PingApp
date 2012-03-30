de387a72e12a423fa62e46679ae421afe7f520f5
otakustay <otakustay@gmail.com>
2012-03-30 17:59:31 +0800

    First implement on PingApp.Repository.NHibernate

--- src/PingApp.Repository.NHibernate/AppRepository.cs
+++ src/PingApp.Repository.NHibernate/AppRepository.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Criterion;
+using PingApp.Entity;
 
 namespace PingApp.Repository.NHibernate {
     public class AppRepository : IAppRepository {
+        private readonly ISession session;
+
+        public AppRepository(ISession session) {
+            this.session = session;
+        }
+
         public ISet<int> FindExists(IEnumerable<int> apps) {
-            throw new NotImplementedException();
+            IList<int> list = session.CreateCriteria<AppBrief>()
+                .Add(Restrictions.InG("Id", apps))
+                .SetProjection(Projections.Property<AppBrief>(a => a.Id))
+                .List<int>();
+
+            return new HashSet<int>(list);
         }
 
-        public ICollection<Entity.App> Retrieve(IEnumerable<int> required) {
-            throw new NotImplementedException();
+        public ICollection<App> Retrieve(IEnumerable<int> required) {
+            ICollection<App> result = session.QueryOver<App>()
+                .Where(Restrictions.InG("Id", required))
+                .List();
+
+            return result;
         }
 
-        public void Save(Entity.App app) {
-            throw new NotImplementedException();
+        public void Save(App app) {
+            session.Save(app);
         }
 
-        public void Update(Entity.App app) {
-            throw new NotImplementedException();
+        public void Update(App app) {
+            session.Update(app);
         }
     }
 }
--- src/PingApp.Repository.NHibernate/AppUpdateRepository.cs
+++ src/PingApp.Repository.NHibernate/AppUpdateRepository.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NHibernate;
+using PingApp.Entity;
 
 namespace PingApp.Repository.NHibernate {
     public class AppUpdateRepository : IAppUpdateRepository {
-        public void Save(Entity.AppUpdate update) {
-            throw new NotImplementedException();
+        private readonly ISession session;
+
+        public AppUpdateRepository(ISession session) {
+            this.session = session;
+        }
+
+        public void Save(AppUpdate update) {
+            session.Save(update);
         }
     }
 }
