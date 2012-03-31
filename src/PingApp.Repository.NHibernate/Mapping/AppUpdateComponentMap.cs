08d973ed4ca89784c2272bd0b9cf1e11232c9074
otakustay <otakustay@gmail.com>
2012-03-31 22:19:36 +0800

    Modify mapping to work

--- src/PingApp.Repository.NHibernate/Mapping/AppUpdateComponentMap.cs
+++ src/PingApp.Repository.NHibernate/Mapping/AppUpdateComponentMap.cs
@@ -8,7 +8,7 @@ using PingApp.Entity;
 namespace PingApp.Repository.NHibernate.Mapping {
     public class AppUpdateComponentMap : ComponentMap<AppUpdate> {
         public AppUpdateComponentMap() {
-            Map(u => u.App).Column("Id");
+            Map(u => u.App).Column("Id").ReadOnly();
             Map(u => u.NewValue).Column("LastValidUpdateNewValue");
             Map(u => u.OldValue).Column("LastValidUpdateOldValue");
             Map(u => u.Time).Column("LastValidUpdateTime");
