2017148a9def901dafc9409b146e6f99c87ea483
otakustay <otakustay@gmail.com>
2012-03-31 13:55:03 +0800

    Add implement to IAppTrackRepository and NHibernateRepositoryModule

---
+++ src/PingApp.Repository.NHibernate/AppTrackRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using PingApp.Entity;
+
+namespace PingApp.Repository.NHibernate {
+    public class AppTrackRepository : IAppTrackRepository {
+        private readonly ISession session;
+
+        public AppTrackRepository(ISession session) {
+            this.session = session;
+        }
+
+        public ICollection<AppTrack> RetrieveByApp(int app) {
+            ICollection<AppTrack> tracks = session.QueryOver<AppTrack>()
+                .Where(t => t.App.Id == app)
+                .List();
+
+            return tracks;
+        }
+
+        public void ResetForApp(int app) {
+            session.CreateSQLQuery("update AppTrack set HasRead = 0 where App = ?App")
+                .SetParameter("?App", app)
+                .ExecuteUpdate();
+        }
+    }
+}
---
+++ src/PingApp.Repository.NHibernate/NHibernateRepositoryModule.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using Ninject.Activation;
+using Ninject.Modules;
+using PingApp.Repository.NHibernate.Mapping;
+using Ninject;
+
+namespace PingApp.Repository.NHibernate {
+    public sealed class NHibernateRepositoryModule : NinjectModule {
+        private const string SESSION_STORE_KEY = "NHIBERNATE_SESSION";
+
+        private readonly IDictionary store;
+
+        public NHibernateRepositoryModule(IDictionary store) {
+            this.store = store;
+        }
+
+        public override void Load() {
+            string connectionString = ConfigurationManager.ConnectionStrings["PingApp"].ConnectionString;
+            ISessionFactory sessionFactory = Fluently.Configure()
+                .Database(MySQLConfiguration.Standard.ConnectionString(connectionString).ShowSql())
+                .Mappings(m => m.FluentMappings.Add<AppUpdateEntityMap>())
+                .Mappings(m => m.FluentMappings.Add<AppEntityMap>())
+                .Mappings(m => m.FluentMappings.Add<AppBriefEntityMap>())
+                .Mappings(m => m.FluentMappings.Add<UserEntityMap>())
+                .Mappings(m => m.FluentMappings.Add<AppTrackEntityMap>())
+                .Mappings(m => m.FluentMappings.Add<DeveloperComponentMap>())
+                .Mappings(m => m.FluentMappings.Add<SellerComponentMap>())
+                .Mappings(m => m.FluentMappings.Add<AppUpdateComponentMap>())
+                .BuildSessionFactory();
+
+            Bind<ISessionFactory>().ToConstant(sessionFactory);
+
+            Bind<ISession>().ToMethod(OpenSession).InSingletonScope();
+            Bind<IAppRepository>().To<AppRepository>();
+            Bind<IAppUpdateRepository>().To<AppUpdateRepository>();
+            Bind<IAppTrackRepository>().To<AppTrackRepository>();
+        }
+
+        private ISession OpenSession(IContext context) {
+            if (!store.Contains(SESSION_STORE_KEY)) {
+                ISessionFactory factory = context.Kernel.Get<ISessionFactory>();
+                ISession session = factory.OpenSession();
+                session.BeginTransaction();
+                store[SESSION_STORE_KEY] = session;
+            }
+            return store[SESSION_STORE_KEY] as ISession;
+        }
+    }
+}
