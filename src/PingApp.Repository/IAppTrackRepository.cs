bf68510200f7d85d6b7d0bc437a58f6c365f2fe5
otakustay <otakustay@gmail.com>
2012-03-31 13:47:55 +0800

    Add repository interface for AppTrack

---
+++ src/PingApp.Repository/IAppTrackRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PingApp.Entity;
+
+namespace PingApp.Repository {
+    public interface IAppTrackRepository {
+
+        // 以下为Schedule使用
+        ICollection<AppTrack> RetrieveByApp(int app);
+
+        void ResetForApp(int app);
+    }
+}
