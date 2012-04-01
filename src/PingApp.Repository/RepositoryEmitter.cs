ff98daf3aa9df47dcd0e3d41bf62818da0cebf43
otakustay <otakustay@gmail.com>
2012-04-01 13:58:45 +0800

    Add RepositoryEmitter to manage all repository as a facade

---
+++ src/PingApp.Repository/RepositoryEmitter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PingApp.Repository {
+    public sealed class RepositoryEmitter {
+        public IAppRepository App { get; private set; }
+
+        public IAppUpdateRepository AppUpdate { get; private set; }
+
+        public IAppTrackRepository AppTrack { get; private set; }
+
+        public RepositoryEmitter(IAppRepository app, 
+            IAppUpdateRepository appUpdate,
+            IAppTrackRepository appTrack) {
+                App = app;
+                AppUpdate = appUpdate;
+                AppTrack = appTrack;
+        }
+    }
+}
