fccaec1163bcd6fac49bad80ed84f731774c1242
otakustay <otakustay@gmail.com>
2012-04-07 14:30:14 +0800

    Add IUserRepository

---
+++ src/PingApp.Repository/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PingApp.Entity;
+
+namespace PingApp.Repository {
+    public interface IUserRepository {
+        User Retrieve(int id);
+    }
+}
