3dc4c5535bc02039aacbef8975d86d6235ad7429
otakustay <otakustay@gmail.com>
2012-04-10 00:23:30 +0800

    Add RevokedApp, apps that cannot be found in search api become revoked app and stored in a different place other than normal apps

---
+++ src/PingApp.Entity/RevokedApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PingApp.Entity {
+    public class RevokedApp : App {
+        public DateTime RevokeTime { get; set; }
+
+        public RevokedApp(App source) {
+            Id = source.Id;
+            Brief = new AppBrief() { Id = source.Id };
+            UpdateFrom(source);
+        }
+    }
+}
