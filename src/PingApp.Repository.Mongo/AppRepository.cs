1b6fe9463931c111c15a7216291dbe51fc9fe2b1
otakustay <otakustay@gmail.com>
2012-04-11 01:30:51 +0800

    Minor changes

--- src/PingApp.Repository.Mongo/AppRepository.cs
+++ src/PingApp.Repository.Mongo/AppRepository.cs
@@ -70,13 +70,13 @@ namespace PingApp.Repository.Mongo {
 
         public RevokedApp Revoke(App app) {
             // 添加下架信息
-            AppUpdate offUpdate = new AppUpdate() {
+            AppUpdate update = new AppUpdate() {
                 App = app.Id,
                 Type = AppUpdateType.Revoke,
                 OldValue = app.Brief.Version + ", " + app.Brief.PriceWithSymbol,
                 Time = DateTime.Now
             };
-            appUpdates.Save(offUpdate);
+            appUpdates.Save(update);
 
             // 从在售应用中移除
             apps.Remove(Query.EQ("_id", app.Id), RemoveFlags.Single);
@@ -97,6 +97,7 @@ namespace PingApp.Repository.Mongo {
         }
 
         public void Resurrect(App app) {
+            // 添加重新上架信息
             AppUpdate update = new AppUpdate() {
                 App = app.Id,
                 NewValue = app.Brief.Version + ", " + app.Brief.PriceWithSymbol,
@@ -105,8 +106,10 @@ namespace PingApp.Repository.Mongo {
             };
             appUpdates.Save(update);
 
+            // 从下架应用中移除
             revokedApps.Remove(Query.EQ("_id", app.Id));
 
+            // 重新加入到应用集合中，并标记最后更新
             app.Brief.LastValidUpdate = update;
             apps.Save(app);
         }
