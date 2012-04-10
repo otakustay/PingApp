719b9153e8c44150e777cb98f9edba10c437dc99
otakustay <otakustay@gmail.com>
2012-04-11 01:16:59 +0800

    Add Test to ActionType

--- src/PingApp.Schedule/ActionType.cs
+++ src/PingApp.Schedule/ActionType.cs
@@ -5,6 +5,6 @@ using System.Text;
 
 namespace PingApp.Schedule {
     enum ActionType {
-        Initialize, RssCheck, Update, FullCheck, Top100Check, Rescue
+        Initialize, RssCheck, Update, FullCheck, Top100Check, Rescue, Test
     }
 }
