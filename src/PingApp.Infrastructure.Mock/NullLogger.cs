b9ce9ff307f1fc32be701566cd71b95ac8e4b980
otakustay <otakustay@gmail.com>
2012-04-21 23:35:01 +0800

    Add NullLogger(Factory) for mock usage

---
+++ src/PingApp.Infrastructure.Mock/NullLogger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingApp.Infrastructure.Mock {
+    sealed class NullLogger : ILogger {
+        public void Log(LogLevel level, string message, params object[] args) {
+        }
+
+        public void LogException(LogLevel level, string message, Exception exception) {
+        }
+
+        public void Trace(string message, params object[] args) {
+        }
+
+        public void TraceException(string message, Exception exception) {
+        }
+
+        public void Debug(string message, params object[] args) {
+        }
+
+        public void DebugException(string message, Exception exception) {
+        }
+
+        public void Info(string message, params object[] args) {
+            throw new NotImplementedException();
+        }
+
+        public void InfoException(string message, Exception exception) {
+        }
+
+        public void Warn(string message, params object[] args) {
+        }
+
+        public void WarnException(string message, Exception exception) {
+        }
+
+        public void Error(string message, params object[] args) {
+        }
+
+        public void ErrorException(string message, Exception exception) {
+        }
+
+        public void Fatal(string message, params object[] args) {
+        }
+
+        public void FatalException(string message, Exception exception) {
+        }
+    }
+}
---
+++ src/PingApp.Infrastructure.Mock/NullLoggerFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingApp.Infrastructure.Mock {
+    public class NullLoggerFactory : ILoggerFactory {
+        private static readonly ILogger instance = new NullLogger();
+
+        public ILogger GetLogger<T>() {
+            return instance;
+        }
+
+        public ILogger GetLogger(Type type) {
+            return instance;
+        }
+
+        public ILogger GetLogger(string name) {
+            return instance;
+        }
+    }
+}
