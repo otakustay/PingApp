2e968ae162f6f9c0e149bc06a8261025cb7d5885
otakustay <otakustay@gmail.com>
2012-05-13 16:08:21 +0800

    Add TrackingApp to combine an app and its user-track info

---
+++ src/PingApp.Web/Models/TrackingApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PingApp.Entity;
+
+namespace PingApp.Web.Models {
+    /// <summary>
+    /// 带用户关注信息的App
+    /// </summary>
+    public class TrackingApp {
+        public AppBrief App { get; private set; }
+
+        public AppTrack Track { get; private set; }
+
+        public TrackingApp(AppBrief app, AppTrack track) {
+            App = app;
+            Track = track;
+        }
+    }
+}
