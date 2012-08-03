54017f52f51a74ee0a9c8df1048cc02ee1077ec7
otakustay <otakustay@gmail.com>
2012-08-03 16:05:05 +0800

    Added MySql repository implement project

---
+++ src/PingApp.Repository.MySql/Map.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using PingApp.Entity;
+
+namespace PingApp.Repository.MySql {
+    public static class Map {
+        public static User ToUser(this IDataRecord record) {
+            throw new NotImplementedException();
+        }
+
+        public static AppBrief ToAppBrief(this IDataRecord record) {
+            throw new NotImplementedException();
+        }
+
+        public static App ToApp(this IDataRecord record) {
+            throw new NotImplementedException();
+        }
+
+        public static App ToRevokedApp(this IDataRecord record) {
+            throw new NotImplementedException();
+        }
+
+        public static AppUpdate ToAppUpdate(this IDataRecord record) {
+            throw new NotImplementedException();
+        }
+
+        public static AppTrack ToAppTrack(this IDataRecord record) {
+            throw new NotImplementedException();
+        }
+    }
+}
