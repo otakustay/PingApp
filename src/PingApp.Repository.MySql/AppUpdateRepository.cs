273f9772b4910771f1a26375033d71dc344f1a22
otakustay <otakustay@gmail.com>
2012-08-07 18:03:08 +0800

    Implemented AppUpdateRepository

--- src/PingApp.Repository.MySql/AppUpdateRepository.cs
+++ src/PingApp.Repository.MySql/AppUpdateRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using MySql.Data.MySqlClient;
@@ -15,11 +16,46 @@ namespace PingApp.Repository.MySql {
         }
 
         public AppUpdateQuery RetrieveByApp(AppUpdateQuery query) {
-            throw new NotImplementedException();
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "select * from AppUpdate where App = ?App and Time <= ?LatestTime";
+            command.Parameters.AddWithValue("?App", query.App);
+            command.Parameters.AddWithValue("?LatestTime", query.LatestTime);
+            if (query.EarliestTime.HasValue) {
+                command.CommandText += " and Time >= ?EarliestTime";
+                command.Parameters.AddWithValue("?EarliestTime", query.EarliestTime);
+            }
+            command.CommandText += ";";
+
+            List<AppUpdate> result = new List<AppUpdate>();
+            using (IDataReader reader = command.ExecuteReader()) {
+                while (reader.Read()) {
+                    AppUpdate update = reader.ToAppUpdate();
+                    result.Add(update);
+                }
+            }
+
+            query.Fill(result);
+            return query;
         }
 
         public void Save(AppUpdate update) {
-            throw new NotImplementedException();
+            update.Id = Guid.NewGuid();
+
+            string sql =
+@"insert into `AppUpdate`(
+    `Id`, `App`, `Time`, `Type`, `OldValue`, `NewValue`
+) values (
+    ?Id, ?App, ?Time, ?Type, ?OldValue, ?NewValue, 
+);";
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = sql;
+            command.Parameters.AddWithValue("?Id", update.Id.ToString("N"));
+            command.Parameters.AddWithValue("?App", update.App);
+            command.Parameters.AddWithValue("Time", update.Time);
+            command.Parameters.AddWithValue("?Type", update.Type);
+            command.Parameters.AddWithValue("?OldValue", update.OldValue);
+            command.Parameters.AddWithValue("?NewValue", update.NewValue);
+            command.ExecuteNonQuery();
         }
 
         public void Dispose() {
