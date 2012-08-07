315da192b1951489ada1bd37d48e85d6e6cf0069
otakustay <otakustay@gmail.com>
2012-08-07 17:57:54 +0800

    Added AppUpdateRepository class

---
+++ src/PingApp.Repository.MySql/AppUpdateRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using PingApp.Entity;
+using PingApp.Repository.Quries;
+
+namespace PingApp.Repository.MySql {
+    public class AppUpdateRepository : IAppUpdateRepository, IDisposable {
+        private readonly MySqlConnection connection;
+
+        public AppUpdateRepository(MySqlConnection connection) {
+            this.connection = connection;
+        }
+
+        public AppUpdateQuery RetrieveByApp(AppUpdateQuery query) {
+            throw new NotImplementedException();
+        }
+
+        public void Save(AppUpdate update) {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose() {
+            connection.Dispose();
+        }
+    }
+}
