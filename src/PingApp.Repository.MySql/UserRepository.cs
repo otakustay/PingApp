4d0469ef44a5d7d74366422eba1f27daca6afc73
otakustay <otakustay@gmail.com>
2012-08-07 18:09:07 +0800

    Implemented Save method for UserRepository

--- src/PingApp.Repository.MySql/UserRepository.cs
+++ src/PingApp.Repository.MySql/UserRepository.cs
@@ -14,7 +14,37 @@ namespace PingApp.Repository.MySql {
         }
 
         public void Save(User user) {
-            throw new NotImplementedException();
+            user.Id = Guid.NewGuid();
+
+            string sql =
+@"insert into `User` (
+    `Id`, `Email`, `Username`, `Password`, `Description`, `Website`,
+    `NotifyOnWishPriceDrop`, `NotifyOnWishFree`, `NotifyOnWishUpdate`,
+    `NotifyOnOwnedUpdate`, `ReceiveSiteUpdates`, `PreferredLanguagePriority`,
+    `Status`, `RegisterTime`
+) values (
+    ?Id, ?Email, ?Username, ?Password, ?Description, ?Website,
+    ?NotifyOnWishPriceDrop, ?NotifyOnWishFree, ?NotifyOnWishUpdate,
+    ?NotifyOnOwnedUpdate, ?ReceiveSiteUpdates, ?PreferredLanguagePriority,
+    ?Status, ?RegisterTime
+);";
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = sql;
+            command.Parameters.AddWithValue("?Id", user.Id.ToString("N"));
+            command.Parameters.AddWithValue("?Email", user.Email);
+            command.Parameters.AddWithValue("?Username", user.Username);
+            command.Parameters.AddWithValue("?Password", user.Password);
+            command.Parameters.AddWithValue("?Description", user.Description);
+            command.Parameters.AddWithValue("?Website", user.Website);
+            command.Parameters.AddWithValue("?NotifyOnWishPriceDrop", user.NotifyOnWishPriceDrop);
+            command.Parameters.AddWithValue("?NotifyOnWishFree", user.NotifyOnWishFree);
+            command.Parameters.AddWithValue("?NotifyOnWishUpdate", user.NotifyOnWishUpdate);
+            command.Parameters.AddWithValue("?NotifyOnOwnedUpdate", user.NotifyOnOwnedUpdate);
+            command.Parameters.AddWithValue("?ReceiveSiteUpdates", user.ReceiveSiteUpdates);
+            command.Parameters.AddWithValue("?PreferredLanguagePriority", user.PreferredLanguagePriority);
+            command.Parameters.AddWithValue("?Status", user.Status);
+            command.Parameters.AddWithValue("?RegisterTime", user.RegisterTime);
+            command.ExecuteNonQuery();
         }
 
         public void Update(User user) {
