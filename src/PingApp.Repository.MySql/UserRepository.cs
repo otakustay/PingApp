9e6a13a2441d1c404a3c888f5b52eff44778c72e
otakustay <otakustay@gmail.com>
2012-08-07 18:05:13 +0800

    Added UserRepository class

---
+++ src/PingApp.Repository.MySql/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using PingApp.Entity;
+
+namespace PingApp.Repository.MySql {
+    public class UserRepository : IUserRepository, IDisposable {
+        private readonly MySqlConnection connection;
+
+        public UserRepository(MySqlConnection connection) {
+            this.connection = connection;
+        }
+
+        public void Save(User user) {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User user) {
+            throw new NotImplementedException();
+        }
+
+        public User Retrieve(Guid id) {
+            throw new NotImplementedException();
+        }
+
+        public User RetrieveByEmail(string email) {
+            throw new NotImplementedException();
+        }
+
+        public User RetrieveByUsername(string username) {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(string email, string username) {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose() {
+            connection.Dispose();
+        }
+    }
+}
